3010c364b12c80c2d6c835714bd32fc62b657173
Abdullah Al Mahfuz <almahfuz777@gmail.com>
2024-05-15 02:19:55 +0600

    build structure

--- app/src/main/java/com/example/tempchat/MainActivity.java
+++ app/src/main/java/com/example/tempchat/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.tempchat;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private String userType;
+    EditText roomIDtextView, roomPassTextView;
+    Button submitBtn, joinBtn, createBtn, backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,83 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+         roomIDtextView = findViewById(R.id.roomID);
+         roomPassTextView = findViewById(R.id.roomPass);
+         submitBtn = findViewById(R.id.submitBtn);
+         joinBtn = findViewById(R.id.selectJoinChat);
+         createBtn = findViewById(R.id.selectCreateChat);
+         backBtn = findViewById(R.id.backBtn);
+
+    }
+
+    public void selectJoinChat(View view) {
+        userType = "join";
+
+        roomIDtextView.setVisibility(View.VISIBLE);
+        roomPassTextView.setVisibility(View.VISIBLE);
+        submitBtn.setVisibility(View.VISIBLE);
+        submitBtn.setVisibility(View.VISIBLE);
+        backBtn.setVisibility(View.VISIBLE);
+
+        //
+        joinBtn.setVisibility(View.GONE);
+        createBtn.setVisibility(View.GONE);
+
+        roomIDtextView.setClickable(true);
+        roomIDtextView.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        roomIDtextView.setHint("Enter Room ID");
+        // append the room id;
+        roomPassTextView.setHint("Enter Password");
+        submitBtn.setText("JOIN ROOM");
+
+    }
+
+    public void selectCreateChat(View view) {
+        userType = "create";
+
+        roomIDtextView.setVisibility(View.VISIBLE);
+        roomPassTextView.setVisibility(View.VISIBLE);
+        submitBtn.setVisibility(View.VISIBLE);
+        submitBtn.setVisibility(View.VISIBLE);
+        backBtn.setVisibility(View.VISIBLE);
+
+        //
+        joinBtn.setVisibility(View.GONE);
+        createBtn.setVisibility(View.GONE);
+
+        roomIDtextView.setClickable(false);
+        roomIDtextView.setInputType(0);
+
+        //generate room id
+        roomIDtextView.setHint("Room ID: ");
+        // append the room id;
+        roomPassTextView.setHint("Set Password");
+        submitBtn.setText("CREATE ROOM");
+    }
+
+    public void submit(View view) {
+        if(userType.equals("join")){
+
+        }
+        else if(userType.equals("create")){
+
+        }
+        else{
+            //error
+        }
+    }
+
+    public void backToMainMenu(View view) {
+        roomIDtextView.setVisibility(View.GONE);
+        roomPassTextView.setVisibility(View.GONE);
+        submitBtn.setVisibility(View.GONE);
+
+        //
+        joinBtn.setVisibility(View.VISIBLE);
+        createBtn.setVisibility(View.VISIBLE);
+        backBtn.setVisibility(View.GONE);
+
     }
 }
